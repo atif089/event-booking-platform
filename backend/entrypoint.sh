2e93d0effa7b6ad04e39edf8181e784663cc5faa
Atif Mohammed <Atif089@gmail.com>
2025-06-22 05:15:24 -0500

    setup app container to run prisma migrations upon start

---
+++ backend/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Exit on error
+set -e
+
+# Set database connection string
+export DATABASE_CONNECTION_STRING="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_DB_HOST}:${POSTGRES_DB_PORT}/${POSTGRES_DB_NAME}"
+
+if [ "$NODE_ENV" = "development" ]; then
+    # persist DATABASE_CONNECTION_STRING by appending to .env file
+    echo "\nDATABASE_CONNECTION_STRING=${DATABASE_CONNECTION_STRING}" >> .env
+    # Run database migrations
+    npx prisma migrate deploy
+fi
+
+# Execute the command passed to this script
+exec "$@"
