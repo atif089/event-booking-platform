d7a0d0d3465ce77addd6a5d59b9795a50f36a3e6
Atif Mohammed <Atif089@gmail.com>
2025-06-23 07:24:22 -0500

    updated layout to accomodate event editor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,14 @@ import { EVENTS_ENDPOINT } from "./config/api";
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <EventList fetchEndpoint={EVENTS_ENDPOINT} />
+      <div className="flex max-w-6xl mx-auto">
+        <div className="w-1/3">
+          <EventList fetchEndpoint={EVENTS_ENDPOINT} />
+        </div>
+        <div className="w-2/3 flex items-center justify-center">
+          <h2 className="text-3xl font-bold mb-4">Event Editor...</h2>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/components/EventList/index.tsx
+++ frontend/src/components/EventList/index.tsx
@@ -18,8 +18,8 @@ const EventList = ({ fetchEndpoint }: { fetchEndpoint: string }) => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Upcoming Events</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <h1 className="text-3xl font-bold mb-4">Events List</h1>
+      <div className="grid grid-cols-1 gap-4 overflow-y-scroll h-[calc(100vh-100px)] pr-4">
         {events.map((event) => (
           <EventCard key={event.id} event={event} />
         ))}
